98e17fff908e52c65d3363d9314c1736e35db19d
panupongkongkate <61161511+panupongkongkate@users.noreply.github.com>
2020-04-19 12:35:08 +0700

    C#(Emgu)simple matching Math program

--- testmatch/Form1.Designer.cs
+++ testmatch/Form1.Designer.cs
@@ -29,12 +29,124 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.imageBox4 = new Emgu.CV.UI.ImageBox();
+            this.imageBox6 = new Emgu.CV.UI.ImageBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox4
+            // 
+            this.imageBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox4.Location = new System.Drawing.Point(26, 51);
+            this.imageBox4.Name = "imageBox4";
+            this.imageBox4.Size = new System.Drawing.Size(664, 559);
+            this.imageBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox4.TabIndex = 2;
+            this.imageBox4.TabStop = false;
+            this.imageBox4.Click += new System.EventHandler(this.imageBox4_Click);
+            // 
+            // imageBox6
+            // 
+            this.imageBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox6.Location = new System.Drawing.Point(696, 9);
+            this.imageBox6.Name = "imageBox6";
+            this.imageBox6.Size = new System.Drawing.Size(730, 720);
+            this.imageBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox6.TabIndex = 2;
+            this.imageBox6.TabStop = false;
+            this.imageBox6.Click += new System.EventHandler(this.imageBox6_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.button4.Location = new System.Drawing.Point(36, 616);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(147, 107);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "ภาพ1";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.button6.Location = new System.Drawing.Point(203, 616);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(147, 107);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "คำนวน";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(268, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 39);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "ภาพต้นฉบับ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(371, 653);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 39);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "คำตอบ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.textBox1.Location = new System.Drawing.Point(484, 642);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 58);
+            this.textBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(1411, 769);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.imageBox6);
+            this.Controls.Add(this.imageBox4);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private Emgu.CV.UI.ImageBox imageBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private Emgu.CV.UI.ImageBox imageBox4;
+        private Emgu.CV.UI.ImageBox imageBox6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- testmatch/Form1.cs
+++ testmatch/Form1.cs
@@ -7,14 +7,260 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using System.Diagnostics;
+using Emgu.CV.CvEnum;
 
 namespace testmatch
 {
     public partial class Form1 : Form
     {
+        OpenFileDialog op1 = new OpenFileDialog();
+        OpenFileDialog op2 = new OpenFileDialog();
+        OpenFileDialog op3 = new OpenFileDialog();
+        Image<Bgr, byte> source = null;
+        Image<Bgr, byte> template = null;
+        Image<Bgr, byte> templatetwo = null;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (op1.ShowDialog() == DialogResult.OK)
+            {
+                source = new Image<Bgr, byte>(op1.FileName);
+                imageBox4.Image = source;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imageBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imageBox5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void imageBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button6_Click_1(object sender, EventArgs e)
+        {
+            int one = 0;
+            int two = 0;
+            int three = 0;
+            bool plus = false;
+            bool del = false;
+            int sum = 0;
+
+            if ((source != null))
+            {
+    
+                List<Rectangle> keep = new List<Rectangle>();
+                Image<Bgr, byte> imageToShow = source.Copy();//ต้องกอป เพื่อเอาไปวาด ตอนถ้าย ๆ
+
+                //-----เลข1---------------------------------------------------------------------
+                Image<Bgr, byte> temgplas = new Image<Bgr, byte>("C:\\Users\\57120042\\Desktop\\testmatch\\testmatch\\เลข1.jpg");
+                Image<Gray, float> result = source.MatchTemplate(temgplas, Emgu.CV.CvEnum.TemplateMatchingType.CcoeffNormed);
+                double[] min, max;
+                Point[] point1, point2;
+                result.MinMax(out min, out max, out point1, out point2);
+                float[,,] match00 = result.Data;
+                for (int x = 0; x < match00.GetLength(1); x++)
+                {
+                    for (int y = 0; y < match00.GetLength(0); y++)
+                    {
+                        double matchsouce = match00[y, x, 0];
+                        if (matchsouce > 0.9)
+                        {
+                            Rectangle bect = new Rectangle(new Point(x, y), temgplas.Size);
+                            imageToShow.Draw(bect, new Bgr(Color.Red), 5);
+                            
+                            keep.Add(bect);
+                            one = 1 ;
+
+                            //MessageBox.Show("come1");
+                        }
+
+                    }
+                }
+
+                //  imageBox6.Image = imageToShow ; // กอปมาจากข้างบนแล้ววาดเอา
+                //-------เลข2----------------------------------------------------------------
+                Image<Bgr, byte> temgplas2 = new Image<Bgr, byte>("C:\\Users\\57120042\\Desktop\\testmatch\\testmatch\\เลข2.jpg");
+                Image<Gray, float> result2 = source.MatchTemplate(temgplas2, Emgu.CV.CvEnum.TemplateMatchingType.CcoeffNormed);
+                double[] min1, max1;
+                Point[] point11, point21;
+                result.MinMax(out min1, out max1, out point11, out point21);
+                float[,,] match001 = result2.Data;
+                for (int x = 0; x < match001.GetLength(1); x++)
+                {
+                    for (int y = 0; y < match001.GetLength(0); y++)
+                    {
+                        double matchsouce = match001[y, x, 0];
+                        if (matchsouce > 0.9)
+                        {
+                            Rectangle bect = new Rectangle(new Point(x, y), temgplas2.Size);
+                            imageToShow.Draw(bect, new Bgr(Color.Red), 5);
+
+                            keep.Add(bect);
+                            two = 2;
+                            //  MessageBox.Show("come");
+                        }
+
+                    }
+                }
+
+                //-------------เลข3------------------------------------------------------------------
+                Image<Bgr, byte> temgplas3 = new Image<Bgr, byte>("C:\\Users\\57120042\\Desktop\\testmatch\\testmatch\\เลข3.jpg");
+                Image<Gray, float> result3 = source.MatchTemplate(temgplas3, Emgu.CV.CvEnum.TemplateMatchingType.CcoeffNormed);
+                double[] min2, max2;
+                Point[] point12, point22;
+                result3.MinMax(out min2, out max2, out point12, out point22);
+                float[,,] match002 = result3.Data;
+                for (int x = 0; x < match002.GetLength(1); x++)
+                {
+                    for (int y = 0; y < match002.GetLength(0); y++)
+                    {
+                        double matchsouce = match002[y, x, 0];
+                        if (matchsouce > 0.9)
+                        {
+                            Rectangle bect = new Rectangle(new Point(x, y), temgplas3.Size);
+                            imageToShow.Draw(bect, new Bgr(Color.Red), 5);
+
+                            keep.Add(bect);
+                            three = 3; 
+                           // MessageBox.Show("come");
+                        }
+
+                    }
+                }
+                //------------บวก-----------------------------------------------------
+                Image<Bgr, byte> temgplas4 = new Image<Bgr, byte>("C:\\Users\\57120042\\Desktop\\testmatch\\testmatch\\บวก.jpg");
+                Image<Gray, float> result4 = source.MatchTemplate(temgplas4, Emgu.CV.CvEnum.TemplateMatchingType.CcoeffNormed);
+                double[] min3, max3;
+                Point[] point13, point23;
+                result3.MinMax(out min3, out max3, out point13, out point23);
+                float[,,] match003 = result4.Data;
+                for (int x = 0; x < match003.GetLength(1); x++)
+                {
+                    for (int y = 0; y < match003.GetLength(0); y++)
+                    {
+                        double matchsouce = match003[y, x, 0];
+                        if (matchsouce > 0.9)
+                        {
+                            Rectangle bect = new Rectangle(new Point(x, y), temgplas4.Size);
+                            imageToShow.Draw(bect, new Bgr(Color.Red), 5);
+
+                            keep.Add(bect);
+                            plus = true;
+                            //MessageBox.Show("come");
+                        }
+
+                    }
+                }
+                //-------ลบ---------------------------------
+                Image<Bgr, byte> temgplas5 = new Image<Bgr, byte>("C:\\Users\\57120042\\Desktop\\testmatch\\testmatch\\ลบ.jpg");
+                Image<Gray, float> result5 = source.MatchTemplate(temgplas5, Emgu.CV.CvEnum.TemplateMatchingType.CcoeffNormed);
+                double[] min4, max4;
+                Point[] point14, point24;
+                result3.MinMax(out min4, out max4, out point14, out point24);
+                float[,,] match004 = result5.Data;
+                for (int x = 0; x < match004.GetLength(1); x++)
+                {
+                    for (int y = 0; y < match004.GetLength(0); y++)
+                    {
+                        double matchsouce = match004[y, x, 0];
+                        if (matchsouce > 0.9)
+                        {
+                            Rectangle bect = new Rectangle(new Point(x, y), temgplas5.Size);
+                            imageToShow.Draw(bect, new Bgr(Color.Red), 5);
+
+                            keep.Add(bect);
+                            del = true ;
+                            //MessageBox.Show("come");
+                        }
+
+                    }
+                }
+
+                //---------การคำนวณ ---------------
+
+ 
+
+                imageBox6.Image = imageToShow; // กอปมาจากข้างบนแล้ววาดเอา
+
+                if (plus == true)
+                {
+                    sum = one + two + three;
+                    textBox1.Text = sum.ToString();
+
+                }
+                else if (del == true)
+                {
+                    if(three == 0)
+                    {
+                        sum =  two - one;
+                        textBox1.Text = sum.ToString();
+
+                    }
+                    else
+                    {
+                        sum = three - two - one;
+                        textBox1.Text = sum.ToString();
+
+                    }
+ 
+
+
+                }
+                else
+                {
+                    MessageBox.Show("ไม่มีการคำนวน");
+                    textBox1.Text = "";
+                }
+
+            }
+
+ 
+        }
+
+
+
+        private void imageBox7_Click(object sender, EventArgs e)
+        {
+
+        }
     }
+
 }
+
+
+
+
+
+
+
+
+
